9938c3442b89379b5cf29471e516c98a947906ab
Daeguen Kwon <dk31415@naver.com>
2025-07-03 14:50:58 +0900

    style: biome 설정 추가 및 포멧팅 설정

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,15 @@ import LoginPage from "./pages/LoginPage";
 import useAuth, { AuthStatus } from "./hooks/useAuth";
 
 function App() {
+    const { isAuthenticated } = useAuth();
 
-	const { isAuthenticated } = useAuth();
-
-	if (isAuthenticated === AuthStatus.LOADING) {
-		return null;
-	}
-	if (isAuthenticated === AuthStatus.UNAUTHORIZED) {
-		return <LoginPage />;
-	}
-	return (
-		<div>
-		</div>
-	);
+    if (isAuthenticated === AuthStatus.LOADING) {
+        return null;
+    }
+    if (isAuthenticated === AuthStatus.UNAUTHORIZED) {
+        return <LoginPage />;
+    }
+    return <div></div>;
 }
 
 export default App;
