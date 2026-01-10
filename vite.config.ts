72d976f2b5e1162f086b307ddc1f215a0bb030ca
Kwon DaeGeun <dk31415@naver.com>
2026-01-10 23:31:07 +0900

    style: 불필요한 주석 제거

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import path from "node:path"; // 추가!
+import path from "node:path";
 
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react(), tailwindcss()],
 	resolve: {
 		alias: {
-			"@": path.resolve(__dirname, "src"), // 이 부분 추가!
+			"@": path.resolve(__dirname, "src"),
 		},
 	},
 });
