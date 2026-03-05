f250acbea4ec796598296f6cf1032de041d00ac6
Kwon DaeGeun <dk31415@naver.com>
2026-03-05 22:32:22 +0900

    style: lint check

--- src/components/study-detail/ApplicationSection/index.tsx
+++ src/components/study-detail/ApplicationSection/index.tsx
@@ -2,8 +2,8 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import type { ApplicationState } from "@/types/application";
 import { type StudyDetail, StudyRecruitmentMethod } from "@/types/study";
 import {
-  getApplicationSectionTitle,
-  isStudyRecruiting,
+    getApplicationSectionTitle,
+    isStudyRecruiting,
 } from "@/utils/studyStatusHelpers";
 import ApplicationForm from "./ApplicationForm";
 import ApprovedApplicationStatus from "./ApprovedApplicationStatus";
@@ -12,99 +12,105 @@ import PendingApplicationStatus from "./PendingApplicationStatus";
 import RejectedApplicationStatus from "./RejectedApplicationStatus";
 
 interface ApplicationSectionProps {
-  study: StudyDetail;
-  isOwner?: boolean;
-  isMember?: boolean;
-  applicationState: ApplicationState;
+    study: StudyDetail;
+    isOwner?: boolean;
+    isMember?: boolean;
+    applicationState: ApplicationState;
 }
 
 export const ApplicationSection = ({
-  study,
-  isOwner = false,
-  isMember = false,
-  applicationState,
+    study,
+    isOwner = false,
+    isMember = false,
+    applicationState,
 }: ApplicationSectionProps) => {
-  const {
-    isApplying,
-    isApplicationModalOpen,
-    userApplicationStatus,
-    setIsApplicationModalOpen,
-    handleApply,
-  } = applicationState;
+    const {
+        isApplying,
+        isApplicationModalOpen,
+        userApplicationStatus,
+        setIsApplicationModalOpen,
+        handleApply,
+    } = applicationState;
 
-  if (isOwner) {
-    return null;
-  }
-
-  const cardTitle = getApplicationSectionTitle(
-    userApplicationStatus,
-    study.recruitmentMethod,
-  );
-
-  const renderApplicationForm = () => {
-    if (study.recruitmentMethod === StudyRecruitmentMethod.APPLICATION) {
-      return (
-        <ApplicationForm
-          isApplying={isApplying}
-          isApplicationModalOpen={isApplicationModalOpen}
-          applicationText={applicationState.applicationText}
-          setIsApplicationModalOpen={setIsApplicationModalOpen}
-          handleApply={handleApply}
-          setApplicationText={applicationState.setApplicationText}
-        />
-      );
-    } else {
-      const recruiting = isStudyRecruiting(study);
-      return (
-        <FirstComeForm
-          isApplying={isApplying}
-          recruiting={recruiting}
-          handleApply={handleApply}
-        />
-      );
+    if (isOwner) {
+        return null;
     }
-  };
 
-  const renderContent = () => {
-    if (isMember) {
-      return <ApprovedApplicationStatus />;
-    }
-    switch (userApplicationStatus) {
-      case "PENDING":
-        return (
-          <PendingApplicationStatus
-            study={study}
-            isApplying={isApplying}
-            isApplicationModalOpen={isApplicationModalOpen}
-            isLoadingApplicationDetail={
-              applicationState.isLoadingApplicationDetail ?? false
-            }
-            editingApplicationText={applicationState.editingApplicationText}
-            setIsApplicationModalOpen={setIsApplicationModalOpen}
-            handleEditApplication={applicationState.handleEditApplication}
-            handleUpdateApplication={applicationState.handleUpdateApplication}
-            setEditingApplicationText={
-              applicationState.setEditingApplicationText
-            }
-          />
-        );
-      case "REJECTED":
-        return <RejectedApplicationStatus />;
-      default:
-        return renderApplicationForm();
-    }
-  };
+    const cardTitle = getApplicationSectionTitle(
+        userApplicationStatus,
+        study.recruitmentMethod
+    );
 
-  return (
-    <Card className="border-gray-200">
-      <CardHeader>
-        <CardTitle className="font-semibold text-gray-900 text-lg">
-          {cardTitle}
-        </CardTitle>
-      </CardHeader>
-      <CardContent className="space-y-4">{renderContent()}</CardContent>
-    </Card>
-  );
+    const renderApplicationForm = () => {
+        if (study.recruitmentMethod === StudyRecruitmentMethod.APPLICATION) {
+            return (
+                <ApplicationForm
+                    isApplying={isApplying}
+                    isApplicationModalOpen={isApplicationModalOpen}
+                    applicationText={applicationState.applicationText}
+                    setIsApplicationModalOpen={setIsApplicationModalOpen}
+                    handleApply={handleApply}
+                    setApplicationText={applicationState.setApplicationText}
+                />
+            );
+        } else {
+            const recruiting = isStudyRecruiting(study);
+            return (
+                <FirstComeForm
+                    isApplying={isApplying}
+                    recruiting={recruiting}
+                    handleApply={handleApply}
+                />
+            );
+        }
+    };
+
+    const renderContent = () => {
+        if (isMember) {
+            return <ApprovedApplicationStatus />;
+        }
+        switch (userApplicationStatus) {
+            case "PENDING":
+                return (
+                    <PendingApplicationStatus
+                        study={study}
+                        isApplying={isApplying}
+                        isApplicationModalOpen={isApplicationModalOpen}
+                        isLoadingApplicationDetail={
+                            applicationState.isLoadingApplicationDetail ?? false
+                        }
+                        editingApplicationText={
+                            applicationState.editingApplicationText
+                        }
+                        setIsApplicationModalOpen={setIsApplicationModalOpen}
+                        handleEditApplication={
+                            applicationState.handleEditApplication
+                        }
+                        handleUpdateApplication={
+                            applicationState.handleUpdateApplication
+                        }
+                        setEditingApplicationText={
+                            applicationState.setEditingApplicationText
+                        }
+                    />
+                );
+            case "REJECTED":
+                return <RejectedApplicationStatus />;
+            default:
+                return renderApplicationForm();
+        }
+    };
+
+    return (
+        <Card className="border-gray-200">
+            <CardHeader>
+                <CardTitle className="font-semibold text-gray-900 text-lg">
+                    {cardTitle}
+                </CardTitle>
+            </CardHeader>
+            <CardContent className="space-y-4">{renderContent()}</CardContent>
+        </Card>
+    );
 };
 
 export default ApplicationSection;
