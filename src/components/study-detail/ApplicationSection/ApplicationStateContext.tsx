1d362348a54f426e09238de901b82d374325e94b
Kwon DaeGeun <101159518+KwonDeaGeun@users.noreply.github.com>
2026-03-08 23:03:36 +0900

    refactor: 지원하기 섹션 코드 구조 전면 리펙토링 (#128)
    * refactor: 지원 섹션 코드 구조 전면 리펙토링
    
    * refactor: FCFS의 약어 의존성에 의해 FirstCome으로 용어 및 파일명 수정
    
    * style: lint check
    
    * refactor: 분할된 span 태그 병합
    
    * refactor: 거절/승인 컴포넌트 공통으로 추출 및 context api로 과도한 상속을 제거
    
    * style: lint check
    
    * refactor: 지원서 스터디일 경우 지원하기 섹션에 정원 초과 시 지원서 작성창 열기/제출 막기 및 수정 성공 전 모달이 닫히는 버그 수정
    
    * refactor: 에러 핸들러 통일 및 모달 클릭 로직 간소화

---
+++ src/components/study-detail/ApplicationSection/ApplicationStateContext.tsx
@@ -0,0 +1,16 @@
+import { createContext, useContext } from "react";
+import type { ApplicationState } from "@/types/application";
+
+const ApplicationStateContext = createContext<ApplicationState | null>(null);
+
+export const useApplicationState = () => {
+    const context = useContext(ApplicationStateContext);
+    if (!context) {
+        throw new Error(
+            "useApplicationState must be used within an ApplicationStateContext.Provider"
+        );
+    }
+    return context;
+};
+
+export default ApplicationStateContext;
