8deb967c276eff922dd55494f8e5b1c759b6338e
Kwon DaeGeun <dk31415@naver.com>
2026-06-19 16:21:12 +0900

    fix: 직접 import하여 빌드 오류 해결

--- src/lib/apiClient.ts
+++ src/lib/apiClient.ts
@@ -1,17 +1,23 @@
 import ky from "ky";
+import type { AfterResponseHook } from "ky/distribution/types/hooks";
 import { useAuthStore } from "@/stores/useAuthStore";
+
+const afterResponse: AfterResponseHook = async (
+  _request,
+  _options,
+  response,
+  _state,
+) => {
+  if (response.status === 401) {
+    useAuthStore.getState().setUnauthorized();
+  }
+  return response;
+};
 
 export const apiClient = ky.create({
   prefixUrl: import.meta.env.VITE_API_URL,
   credentials: "include",
   hooks: {
-    afterResponse: [
-      ((async (_request, _options, response, _state) => {
-        if (response.status === 401) {
-          useAuthStore.getState().setUnauthorized();
-        }
-        return response;
-      }) as ky.AfterResponseHook),
-    ],
+    afterResponse: [afterResponse],
   },
 });
