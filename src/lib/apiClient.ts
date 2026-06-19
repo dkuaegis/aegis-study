3e30dad17b4644712cb737d7bb2eb074a02d90e4
Kwon DaeGeun <dk31415@naver.com>
2026-06-19 16:24:46 +0900

    fix: 타입 에러 우회

--- src/lib/apiClient.ts
+++ src/lib/apiClient.ts
@@ -1,23 +1,17 @@
 import ky from "ky";
-import type { AfterResponseHook } from "ky/distribution/types/hooks";
 import { useAuthStore } from "@/stores/useAuthStore";
 
-const afterResponse: AfterResponseHook = async (
-  _request,
-  _options,
-  response,
-  _state,
-) => {
-  if (response.status === 401) {
-    useAuthStore.getState().setUnauthorized();
-  }
-  return response;
-};
-
 export const apiClient = ky.create({
-  prefixUrl: import.meta.env.VITE_API_URL,
+  prefix: import.meta.env.VITE_API_URL,
   credentials: "include",
   hooks: {
-    afterResponse: [afterResponse],
+    afterResponse: [
+      (async ({ response }) => {
+        if (response.status === 401) {
+          useAuthStore.getState().setUnauthorized();
+        }
+        return response;
+      }) as any,
+    ],
   },
 });
