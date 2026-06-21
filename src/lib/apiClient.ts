7ddf0117400e6a8177c06668ef614cc760fc9b46
Kwon DaeGeun <dk31415@naver.com>
2026-06-21 21:06:43 +0900

    fix: any 사용한 부분 제거 및 ky 버전에 맞는 명칭으로 수정

--- src/lib/apiClient.ts
+++ src/lib/apiClient.ts
@@ -2,16 +2,16 @@ import ky from "ky";
 import { useAuthStore } from "@/stores/useAuthStore";
 
 export const apiClient = ky.create({
-  prefix: import.meta.env.VITE_API_URL,
+  prefixUrl: import.meta.env.VITE_API_URL,
   credentials: "include",
   hooks: {
     afterResponse: [
-      (async ({ response }) => {
+      async (_request, _options, response) => {
         if (response.status === 401) {
           useAuthStore.getState().setUnauthorized();
         }
         return response;
-      }) as any,
+      },
     ],
   },
 });
